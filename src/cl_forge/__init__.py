aaf4f5aa465f857f3f585157c2df818eb73d6972
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-01-26 15:13:12 -0300

    ✨ feat(imports): add core components to module exports
    - include endpoints, impl, schemas, and timing in __all__ exports
    - add rs_cmf and rs_verify to new __init__.py file

--- src/cl_forge/__init__.py
+++ src/cl_forge/__init__.py
@@ -4,7 +4,7 @@ import importlib.metadata
 
 from cl_forge import cmf, exceptions, utils, verify
 
-__all__ = ("cmf", "exceptions", "utils", "verify")
+__all__ = ("cmf", "exceptions", "utils", "verify",)
 
 try:
     __version__ = importlib.metadata.version("cl-forge")
