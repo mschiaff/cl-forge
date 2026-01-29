c5003d3d4c12999ed86f6a5b3f8b7783d5465d8a
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-01-29 16:22:56 -0300

    ♻️ refactor(api): remove unused import from typing in rs_cmf.pyi

--- src/cl_forge/core/impl/rs_cl_forge/rs_cmf.pyi
+++ src/cl_forge/core/impl/rs_cl_forge/rs_cmf.pyi
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Any, Literal, overload
+    from typing import Literal, overload
 
 class CmfClient:
     """
