69e3c551322cf8f1dbafd3ff7a87768aafbb9160
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-04-17 16:52:39 -0400

    feat: implement BaseCmfClient with synchronous and asynchronous get methods

--- rust/cmf/src/lib.rs
+++ rust/cmf/src/lib.rs
@@ -8,6 +8,86 @@ use pyo3::types::{PyString, PyAny};
 use base::enums::ResponseFormat;
 
 
+fn build_response<'py>(
+    py: Python<'py>,
+    fmt: ResponseFormat,
+    body: &str
+) -> PyResult<Bound<'py, PyAny>> {
+    match fmt {
+        ResponseFormat::Json => base::json_to_dict(py, body),
+        ResponseFormat::Xml => Ok(PyString::new(py, body).into_any()),
+    }
+}
+
+
+#[pyclass(subclass, module = "cl_forge.core.impl.rs_cl_forge.rs_cmf")]
+struct BaseCmfClient {
+    inner: client::BaseCmfClient,
+}
+
+#[pymethods]
+impl BaseCmfClient {
+    #[new]
+    fn new(api_key: &str) -> PyResult<Self> {
+        let inner = client::BaseCmfClient::new(api_key)?;
+        
+        Ok(Self { inner })
+    }
+
+    #[getter]
+    fn base_url(&self) -> String {
+        self.inner.client.base_url.clone()
+    }
+
+    #[getter]
+    fn api_key(&self) -> String {
+        self.inner.client.api_key.clone()
+    }
+
+    #[pyo3(signature = (path, fmt="json"))]
+    fn get<'py>(
+        &self,
+        py: Python<'py>,
+        path: &str,
+        fmt: Option<&str>
+    ) -> PyResult<Bound<'py, PyAny>> {
+        let path = path.to_string();
+        let fmt = ResponseFormat::try_from(fmt)?;
+        let response = self.inner.get(path, fmt)?;
+
+        build_response(py, fmt, &response)
+    }
+
+    #[pyo3(signature = (path, fmt="json"))]
+    fn aget<'py>(
+        &self,
+        py: Python<'py>,
+        path: &str,
+        fmt: Option<&str>
+    ) -> PyResult<Bound<'py, PyAny>> {
+        let inner = self.inner.clone();
+        let path = path.to_string();
+        let fmt = ResponseFormat::try_from(fmt)?;
+
+        pyo3_async_runtimes::tokio::future_into_py(py, async move {
+            let response = inner.aget(path, fmt).await?;
+            
+            Python::attach(|py|{
+                build_response(py, fmt, &response)
+                    .map(|object| object.unbind())
+            })
+        })
+    }
+
+    fn __repr__(&self) -> String {
+        format!(
+            "BaseCmfClient(base_url='{}')",
+            self.base_url(),
+        )
+    }
+}
+
+
 #[pyclass]
 struct CmfClient {
     client: native::CmfClient,
@@ -66,5 +146,6 @@ impl CmfClient {
 #[pymodule]
 pub fn rs_cmf(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<CmfClient>()?;
+    m.add_class::<BaseCmfClient>()?;
     Ok(())
 }
