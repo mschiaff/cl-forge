6b16898787989b3bc575ed91b9c3e58ad8d2f44b
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-04-17 17:38:54 -0400

    refactor: remove CmfClient implementation and associated native module

--- rust/cmf/src/lib.rs
+++ rust/cmf/src/lib.rs
@@ -1,5 +1,4 @@
 mod constants;
-mod native;
 mod client;
 
 use pyo3::prelude::*;
@@ -91,64 +90,8 @@ impl BaseCmfClient {
 }
 
 
-#[pyclass]
-struct CmfClient {
-    client: native::CmfClient,
-}
-
-#[pymethods]
-impl CmfClient {
-    #[new]
-    fn new(api_key: &str) -> PyResult<Self> {
-        let client = native::CmfClient::new(api_key)?;
-        
-        Ok(Self { client })
-    }
-    
-    #[getter]
-    fn base_url(&self) -> String {
-        self.client.base.base_url.clone()
-    }
-    
-    #[getter]
-    fn api_key(&self) -> String {
-        self.client.base.api_key.clone()
-    }
-
-    //noinspection DuplicatedCode
-    #[pyo3(signature = (path, fmt="json"))]
-    fn get<'py>(
-            &self,
-            py: Python<'py>,
-            path: String,
-            fmt: Option<&str>
-    ) -> PyResult<Bound<'py, PyAny>> {
-        let fmt = ResponseFormat::try_from(fmt)?;
-        let body: String = self.client.get(path, fmt)?;
-        
-        match fmt {
-            ResponseFormat::Json => {
-                Ok(base::json_to_dict(py, &body)?)
-            }
-            ResponseFormat::Xml => {
-                Ok(PyString::new(py, &body).into_any())
-            }
-        }
-    }
-
-    fn __repr__(&self) -> String {
-        format!(
-            "CmfClient(api_key='{}', base_url='{}')",
-            self.api_key(),
-            self.base_url(),
-        )
-    }
-}
-
-
 #[pymodule]
 pub fn rs_cmf(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<CmfClient>()?;
     m.add_class::<BaseCmfClient>()?;
     Ok(())
 }
