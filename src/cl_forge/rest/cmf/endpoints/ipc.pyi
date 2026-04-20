adfa8e04ed0d830ed92f5623b876f586363483c7
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-04-20 10:12:53 -0400

    refactor: import RangeMode and enhance ipc_range_endpoint overloads

--- src/cl_forge/rest/cmf/endpoints/ipc.pyi
+++ src/cl_forge/rest/cmf/endpoints/ipc.pyi
@@ -3,6 +3,7 @@ from typing import Never, overload
 from cl_forge.core.compat import deprecated
 from cl_forge.rest.cmf.endpoints.base import CmfEndpoint
 from cl_forge.rest.cmf.schemas import IpcRecord, ListIpcRecord
+from cl_forge.rest.cmf.types import RangeMode
 
 @overload
 @deprecated("Month cannot be specified without year.")
@@ -30,3 +31,28 @@ def ipc_endpoint(
         year: int | None = ...,
         month: int | None = ...
 ) -> CmfEndpoint[IpcRecord] | CmfEndpoint[ListIpcRecord]: ...
+
+
+@overload
+def ipc_range_endpoint(
+        *,
+        start_year: int,
+        start_month: int | None = ...,
+        mode: RangeMode = ...
+) -> CmfEndpoint[ListIpcRecord]: ...
+@overload
+def ipc_range_endpoint(
+        *,
+        start_year: int,
+        end_year: int,
+        mode: RangeMode = ...
+) -> CmfEndpoint[ListIpcRecord]: ...
+@overload
+def ipc_range_endpoint(
+        *,
+        start_year: int,
+        start_month: int,
+        end_year: int,
+        end_month: int,
+        mode: RangeMode = ...
+) -> CmfEndpoint[ListIpcRecord]: ...
