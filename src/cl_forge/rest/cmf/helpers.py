b2bf00e674d60d4c114500d0af63dd6152472402
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-04-21 16:59:30 -0400

    refactor(helpers): add is_range_between_months function for validating date ranges

--- src/cl_forge/rest/cmf/helpers.py
+++ src/cl_forge/rest/cmf/helpers.py
@@ -13,3 +13,39 @@ def validate_month(month: int, word: Literal["start", "end"] | None = None) -> N
 def validate_day(day: int) -> None:
     if not (1 <= day <= 31):
         raise ValueError("Day must be between 1 and 31.")
+
+
+def is_range_between_months(
+        start_year: int,
+        start_month: int | None,
+        end_year: int | None,
+        end_month: int | None,
+        alt: str
+) -> bool:
+    if not end_year:
+            raise ValueError(
+                "End year must be specified for 'between' mode."
+            )
+    if start_year > end_year:
+        raise ValueError(
+            "Start year cannot be greater than end year for 'between' mode."
+        )
+    if start_month and not end_month:
+        raise ValueError(
+            "End month must be specified if start month is specified for 'between' mode."
+        )
+    if not start_month and end_month:
+        raise ValueError(
+            "Start month must be specified if end month is specified for 'between' mode."
+        )
+    if start_month and end_month:
+        if (start_year, start_month) > (end_year, end_month):
+            raise ValueError(
+                "Start date cannot be greater than end date for 'between' mode."
+            )
+        if (start_year, start_month) == (end_year, end_month):
+            raise ValueError(
+                f"For individual month query, use the {alt!r} method."
+            )
+        return True
+    return False
