cf2408bd46b49dc233deb10700816efce3a3f971
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-04-24 08:54:39 -0400

    refactor(uf): replace ModeType with BoundedMode for improved mode handling in uf endpoints

--- src/cl_forge/rest/cmf/endpoints/ipc.py
+++ src/cl_forge/rest/cmf/endpoints/ipc.py
@@ -2,13 +2,17 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from cl_forge.core.types import EndpointEnum, ModeEnum
 from cl_forge.rest.cmf import helpers
 from cl_forge.rest.cmf.endpoints.base import CmfEndpoint
 from cl_forge.rest.cmf.schemas import IpcRecord, ListIpcRecord
-from cl_forge.rest.cmf.types import ModeType
+from cl_forge.rest.cmf.types import BoundedMode
 
 if TYPE_CHECKING:
     from cl_forge.core.types import RangeMode
+
+
+IPC_MODE: BoundedMode = BoundedMode(EndpointEnum.IPC)
 
 
 def ipc_endpoint(
@@ -17,13 +21,13 @@ def ipc_endpoint(
         month: int | None = None
 ) -> CmfEndpoint[IpcRecord] | CmfEndpoint[ListIpcRecord]:
     helpers.validate_month(month) if month else None
-    
+
     path = (
         f"/ipc/{year}/{month}" if year and month
             else f"/ipc/{year}" if year
             else "/ipc"
     )
-    
+
     if year and not month:
         return CmfEndpoint(path=path, model=ListIpcRecord)
     if not year and month:
@@ -42,20 +46,25 @@ def ipc_range_endpoint(
     helpers.validate_month(start_month, "start") if start_month else None
     helpers.validate_month(end_month, "end") if end_month else None
 
-    if isinstance(mode, str): # type: ignore
-        # ModeType will raise ValueError on invalid type
-        # or value, so we don't need to check for that here.
-        _mode = ModeType(mode)
+    # ModeType will raise ValueError on invalid type
+    # or value, so we don't need to check for that here.
+    _mode = IPC_MODE(mode)
 
-    if _mode in (ModeType.AFTER, ModeType.BEFORE):
+    if _mode.type in (ModeEnum.AFTER, ModeEnum.BEFORE):
         if not start_month:
             path = f"{_mode.path}/{start_year}"
             return CmfEndpoint(path=path, model=ListIpcRecord)
         path = f"{_mode.path}/{start_year}/{start_month}"
         return CmfEndpoint(path=path, model=ListIpcRecord)
 
-    if _mode is ModeType.BETWEEN:
-        if helpers.is_range_between_months(start_year, start_month, end_year, end_month, "ipc"):
+    if _mode.type is ModeEnum.BETWEEN:
+        if helpers.is_range_between_months(
+                start_year,
+                start_month,
+                end_year,
+                end_month,
+                "ipc"
+        ):
             path = f"{_mode.path}/{start_year}/{start_month}/{end_year}/{end_month}"
             return CmfEndpoint(path=path, model=ListIpcRecord)
 
