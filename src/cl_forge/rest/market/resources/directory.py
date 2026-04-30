9fb39726c55a81ef60b8f83e237ef1c843abb4ba
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-04-30 15:01:23 -0400

    docs(market): enhance documentation for SuppliersResource and BuyersResource search methods

--- src/cl_forge/rest/market/resources/directory.py
+++ src/cl_forge/rest/market/resources/directory.py
@@ -13,6 +13,30 @@ class SuppliersResource(BaseDirectoryResource[SuppliersResult, SupplierQuery]):
             ignore_root: bool = False,
             only_record: bool = False,
     ) -> SupplierRecord | SuppliersDirectory | SuppliersResult:
+        """
+        Search for suppliers based on the provided RUT.
+
+        Parameters
+        ----------
+        rut : str
+            The RUT of the supplier to search for.
+        ignore_root : bool, optional
+            Whether to ignore the response metadata object, by default False
+        only_record : bool, optional
+            Whether to return only the record, by default False
+
+        Returns
+        -------
+        SupplierRecord | SuppliersDirectory | SuppliersResult
+            The result of the search, which can be a single supplier record,
+            a directory of suppliers, or the full search result.
+
+        Raises
+        ------
+        BadStatus
+            If the the searched supplier is not found or any other error
+            occurs during the search.
+        """
         query = SupplierQuery(rut=rut)
         response =  self._search(query)
 
@@ -26,4 +50,12 @@ class SuppliersResource(BaseDirectoryResource[SuppliersResult, SupplierQuery]):
 
 class BuyersResource(BaseDirectoryResource[BuyersResult, BuyerQuery]):
     def search(self) -> BuyersResult:
+        """
+        Search for all buyers in the directory.
+
+        Returns
+        -------
+        BuyersResult
+            The result of the search, which contains all buyers in the directory.
+        """
         return self._search()
