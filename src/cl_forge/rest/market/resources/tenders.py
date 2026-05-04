80adec391f723efd644e3bd83cd55d22caa73d9d
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-05-04 19:29:38 -0400

    feat(market): simplify TenderQuery method calls for improved readability

--- src/cl_forge/rest/market/resources/tenders.py
+++ src/cl_forge/rest/market/resources/tenders.py
@@ -19,53 +19,27 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         query = TenderQuery(tender_code=tender_code)
         return self._get_details(query)
 
-    def active(self) -> Tender:
-        query = TenderQuery(
-            status=TenderStatus.others.ACTIVE,
-            allow_others=True
-        )
-        return self._get_tenders(query)
-
-    def all(self, date: DateLike | None = None) -> Tender:
-        query = TenderQuery(
-            status=TenderStatus.others.ALL,
-            allow_others=True,
-            date=date,
-        )
-        return self._get_tenders(query)
-
     def by_date(self, date: DateLike) -> Tender:
         query = TenderQuery(date=date)
         return self._get_tenders(query)
 
-    def by_status(self,
-            status: TenderStatusLike,
-            *,
-            date: DateLike | None = None
-    ) -> Tender:
+    def active(self) -> Tender:
+        query = TenderQuery(status=TenderStatus.others.ACTIVE, allow_others=True)
+        return self._get_tenders(query)
+
+    def all(self, date: DateLike | None = None) -> Tender:
+        query = TenderQuery(status=TenderStatus.others.ALL, allow_others=True, date=date)
+        return self._get_tenders(query)
+
+    def by_buyer(self, buyer_code: str | int, *, date: DateLike | None = None) -> Tender:
+        query = TenderQuery(date=date, buyer_code=buyer_code)
+        return self._get_tenders(query)
+
+    def by_status(self, status: TenderStatusLike, *, date: DateLike | None = None) -> Tender:
         query = TenderQuery(status=status, date=date)
         return self._get_tenders(query)
 
-    def by_buyer(
-            self,
-            buyer_code: str | int,
-            *,
-            date: DateLike | None = None,
-    ) -> Tender:
-        query = TenderQuery(
-            date=date,
-            buyer_code=buyer_code,
-        )
-        return self._get_tenders(query)
 
-    def by_supplier(
-            self,
-            supplier_code: str | int,
-            *,
-            date: DateLike | None = None,
-    ) -> Tender:
-        query = TenderQuery(
-            date=date,
-            supplier_code=supplier_code,
-        )
+    def by_supplier(self, supplier_code: str | int, *, date: DateLike | None = None) -> Tender:
+        query = TenderQuery(date=date, supplier_code=supplier_code)
         return self._get_tenders(query)
