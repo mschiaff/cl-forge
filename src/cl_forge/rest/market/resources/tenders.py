879e7208fcb9e1b1f7e662667052ca014534413a
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-05-05 14:22:46 -0400

    feat(tenders): add detailed docstrings and examples for tender methods

--- src/cl_forge/rest/market/resources/tenders.py
+++ src/cl_forge/rest/market/resources/tenders.py
@@ -20,6 +20,15 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         -------
         Tender
             A collection of all tenders for today, regardless of their status.
+
+        Examples
+        --------
+        ```python
+        from cl_forge import MarketClient
+
+        client = MarketClient("your_api_key")
+        response = client.tenders.today()
+        ```
         """
         return self._get_tenders()
 
@@ -36,6 +45,15 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         -------
         TenderDetails
             The details of the specified tender.
+
+        Examples
+        --------
+        ```python
+        from cl_forge import MarketClient
+
+        client = MarketClient("your_api_key")
+        response = client.tenders.details("1003473-14-LR26")
+        ```
         """
         query = TenderQuery(tender_code=tender_code)
         return self._get_details(query)
@@ -54,6 +72,15 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         -------
         Tender
             A collection of tenders for the specified date.
+
+        Examples
+        --------
+        ```python
+        from cl_forge import MarketClient
+
+        client = MarketClient("your_api_key")
+        response = client.tenders.by_date("2024-01-01")
+        ```
         """
         query = TenderQuery(date=date)
         return self._get_tenders(query)
@@ -66,6 +93,15 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         -------
         Tender
             A collection of active tenders.
+
+        Examples
+        --------
+        ```python
+        from cl_forge import MarketClient
+
+        client = MarketClient("your_api_key")
+        response = client.tenders.active()
+        ```
         """
         query = TenderQuery(status=TenderStatus.others.ACTIVE, allow_others=True)
         return self._get_tenders(query)
@@ -84,6 +120,15 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         -------
         Tender
             A collection of all tenders, optionally filtered by date.
+
+        Examples
+        --------
+        ```python
+        from cl_forge import MarketClient
+
+        client = MarketClient("your_api_key")
+        response = client.tenders.all(date="2024-01-01")
+        ```
         """
         query = TenderQuery(status=TenderStatus.others.ALL, allow_others=True, date=date)
         return self._get_tenders(query)
@@ -95,7 +140,7 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         Parameters
         ----------
         buyer_code : str | int
-            The code of the buyer to filter tenders by.
+            The code of the buyer (public entity) to filter tenders by.
         date : DateLike, optional
             The date to filter tenders by. Can be a `datetime.datetime`,
             `datetime.date`, or an ISO format string (yyyy-mm-dd).
@@ -105,6 +150,30 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         Tender
             A collection of tenders for the specified buyer, optionally
             filtered by date.
+
+        Examples
+        --------
+        If you already have the buyer code:
+
+        ```python
+        from cl_forge import MarketClient
+
+        client = MarketClient("your_api_key")
+        response = client.tenders.by_buyer(12345, date="2024-01-01")
+        ```
+
+        If you don't have the buyer code, you can search for it by name using
+        the buyers resource, e.g. `client.buyers.search()`, which will return
+        all the available buyer records. Then you can filter those records by name:
+
+        ```python
+        from cl_forge import MarketClient
+
+        client = MarketClient("your_api_key")
+        buyers = client.buyers.search()
+        buyer = buyers.contains("UNIVERSIDAD DE CHILE").root[0]
+        response = client.tenders.by_buyer(buyer.code, date="2024-01-01")
+        ```
         """
         query = TenderQuery(date=date, buyer_code=buyer_code)
         return self._get_tenders(query)
@@ -128,6 +197,26 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         Tender
             A collection of tenders for the specified status, optionally
             filtered by date.
+
+        Examples
+        --------
+        Using a string for the status parameter:
+
+        ```python
+        from cl_forge import MarketClient
+
+        client = MarketClient("your_api_key")
+        response = client.tenders.by_status("Adjudicada", date="2026-05-04")
+        ```
+
+        Using a :class:`TenderStatus` value for the status parameter:
+
+        ```python
+        from cl_forge import MarketClient, TenderStatus
+
+        client = MarketClient("your_api_key")
+        response = client.tenders.by_status(TenderStatus.AWARDED, date="2026-05-04")
+        ```
         """
         query = TenderQuery(status=status, date=date)
         return self._get_tenders(query)
@@ -149,6 +238,26 @@ class TendersResource(BaseTendersResource[Tender, TenderDetails]):
         Tender
             A collection of tenders for the specified supplier, optionally
             filtered by date.
+
+        Notes
+        -----
+        - If the given supplier code has no tenders for the specified date
+        (or no date is specified), an empty collection will be returned rather
+        than an error.
+        - If you don't have the supplier code, you can search for it by RUT using
+        the suppliers resource, e.g. `client.suppliers.search(...)`, which will
+        return the supplier record matching that RUT (if any).
+
+        Examples
+        --------
+        If you don't have the supplier code, you can search for it by RUT:
+
+        ```python
+        from cl_forge import MarketClient
+
+        supplier = client.suppliers.search("70017820-K", only_record=True)
+        response = client.tenders.by_supplier(supplier.code, date="2026-05-04")
+        ```
         """
         query = TenderQuery(date=date, supplier_code=supplier_code)
         return self._get_tenders(query)
