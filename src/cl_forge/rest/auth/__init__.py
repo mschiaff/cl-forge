9b3f9dd9bfd9d8b1bb6e380584c7bd11908a532a
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-05-07 11:15:04 -0400

    refactor(auth): Import ApiKeyCredentials and add to __all__ for better accessibility

--- src/cl_forge/rest/auth/__init__.py
+++ src/cl_forge/rest/auth/__init__.py
@@ -1,9 +1,10 @@
-from .base import CredentialsProvider
+from .base import ApiKeyCredentials, CredentialsProvider
 from .enums import CredentialScope
 from .exceptions import DotEnvCredentialsError, EnvCredentialsError, MissingCredentialsError
 from .providers import DotEnvCredentials, EnvCredentials, StaticCredentials
 
 __all__ = (
+    "ApiKeyCredentials",
     "CredentialScope",
     "CredentialsProvider",
     "DotEnvCredentials",
