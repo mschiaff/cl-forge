f3ffd68c20276bf58c76f891956287184f06c6c3
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-05-07 09:51:27 -0400

    fix(auth): Add missing exception imports to the auth module

--- src/cl_forge/rest/auth/__init__.py
+++ src/cl_forge/rest/auth/__init__.py
@@ -1,11 +1,15 @@
 from .base import CredentialsProvider
 from .enums import CredentialScope
+from .exceptions import DotEnvCredentialsError, EnvCredentialsError, MissingCredentialsError
 from .providers import DotEnvCredentials, EnvCredentials, StaticCredentials
 
 __all__ = (
     "CredentialScope",
     "CredentialsProvider",
     "DotEnvCredentials",
+    "DotEnvCredentialsError",
     "EnvCredentials",
+    "EnvCredentialsError",
+    "MissingCredentialsError",
     "StaticCredentials",
 )
